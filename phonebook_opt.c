ebca2be088e6041ef6fced91eb454772ca7f9f2e
Pei Hsuan Hung <afcidk@gmail.com>
2018-03-01 23:37:16 +0800

    Implement code in _opt version
    Decrease entry size from 136 bytes to 32 bytes
    Malloc memory for data such as "email" and "phone" in phonebook

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "phonebook_opt.h"
 
@@ -6,11 +8,22 @@
 entry *findName(char lastName[], entry *pHead)
 {
     /* TODO: implement */
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
     /* TODO: implement */
-    return NULL;
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e->pData = (phonebookData *) malloc(sizeof(phonebookData));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    return e;
 }
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,9 +5,9 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+
+typedef struct __OTHER_PHONE_BOOK_DATA {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,7 +17,12 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} phonebookData;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
     struct __PHONE_BOOK_ENTRY *pNext;
+    phonebookData *pData;
 } entry;
 
 entry *findName(char lastName[], entry *pHead);
